a00e89351c8d03eba2240e63812093c93f9efc8d
hisasue <thisasue@redhat.com>
2018-07-13 00:52:55 +0000

    20180712

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -27,3 +27,11 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
 # To be Implemented by Student
+DIR=$(dirname $0)
+oc project ${GUID}-jenkins
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
+#cd $DIR/../templates/jenkins-slave-appdev/
+#docker build . -t docker-registry-default.apps.na39.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
+#docker login -u wkulhane-redhat.com -p $(oc whoami -t) docker-registry-default.apps.na39.openshift.opentlc.com
+#docker push docker-registry-default.apps.na39.openshift.opentlc.com/xyz-jenkins/jenkins-slave-maven-appdev:v3.9
+oc create -f ../templates/bc-jenkins-slave.yaml

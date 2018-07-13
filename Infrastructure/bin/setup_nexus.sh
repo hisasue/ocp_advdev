0d62b4a47ae1945feafcf619c73f4ab7b22ee478
takahiro hisasue <thisasue-redhat.com@bastion.5bb9.example.opentlc.com>
2018-07-13 00:52:55 +0000

    20180712

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -8,6 +8,27 @@ fi
 
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
+oc project $GUID-nexus
+oc new-app docker.io/sonatype/nexus3:latest
+oc expose svc nexus3
+oc rollout pause dc nexus3
+oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
+oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: nexus-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi" | oc create -f -
+
+oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
+oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
+oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
+oc rollout resume dc nexus3
 
 # Code to set up the Nexus. It will need to
 # * Create Nexus
@@ -17,15 +38,23 @@ echo "Setting up Nexus in project $GUID-nexus"
 # Hint: Make sure to wait until Nexus if fully up and running
 #       before configuring nexus with repositories.
 #       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
+ while : ; do
+   echo "Checking if Nexus is Ready..."
+   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
+   [[ "$?" == "1" ]] || break
+   echo "...no. Sleeping 10 seconds."
+   sleep 10
+ done
 
 # Ideally just calls a template
 # oc new-app -f ../templates/nexus.yaml --param .....
 
 # To be Implemented by Student
+
+curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+chmod +x setup_nexus3.sh
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+rm setup_nexus3.sh
+
+oc expose dc nexus3 --port=5000 --name=nexus-registry
+oc create route edge nexus-registry --service=nexus-registry --port=5000

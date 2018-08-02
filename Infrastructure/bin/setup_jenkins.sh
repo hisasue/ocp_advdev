7db3b33947e694deaa80734f57e983f99fcd063d
hisasue <thisasue@redhat.com>
2018-08-02 04:41:24 +0000

    changed building Jenkins slave

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -33,20 +33,22 @@ oc project ${GUID}-jenkins
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -e GUID=${GUID} -e REPO=${REPO} -e CLUSTER=${CLUSTER} -n ${GUID}-jenkins
 #yum install -y docker
 #cd $DIR/../templates/jenkins-slave-appdev/
-oc new-build --name=jenkins-slave-maven-appdev --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && yum clean all\nUSER 1001' -n ${GUID}-jenkins && \
-  sleep 10 && \
-  oc tag jenkins-slave-maven-appdev:latest jenkins-slave-maven-appdev:v3.9 -n ${GUID}-jenkins
+#oc new-build --name=jenkins-slave-maven-centos --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && yum clean all\nUSER 1001' -n ${GUID}-jenkins && \
+oc new-build --name=jenkins-slave-maven-appdev-build --to="docker-registry-default.apps.na39.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9" --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && yum clean all\nUSER 1001' -n ${GUID}-jenkins && \
+
+#  sleep 10 && \
+#oc tag jenkins-slave-maven-centos7:v3.9 jenkins-slave-maven-appdev:v3.9 -n ${GUID}-jenkins
 #cd ./Infrastructure/templates/jenkins-slave-appdev/
 #docker build . -t docker-registry-default.apps.na39.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 #docker login -u thisasue-redhat.com -p $(oc whoami -t) docker-registry-default.apps.na39.openshift.opentlc.com
 #docker push docker-registry-default.apps.na39.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 #cd ../
-oc process -f ./Infrastructure/templates/bc-jenkins-slave.yaml \
-  -p GUID=${GUID} \
-  -p REPO=${REPO} \
-  -p CLUSTER=${CLUSTER} \
-  -n ${GUID}-jenkins \
-  | oc create -f -
+#oc process -f ./Infrastructure/templates/bc-jenkins-slave.yaml \
+#  -p GUID=${GUID} \
+#  -p REPO=${REPO} \
+#  -p CLUSTER=${CLUSTER} \
+#  -n ${GUID}-jenkins \
+#  | oc create -f -
 
 oc process -f ./Infrastructure/templates/bc-app.yaml \
   -p GUID=${GUID} \
